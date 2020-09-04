022b4b48f24a28e88d532bdc87fdcc5997798d98
SBeezy1010 <supremeburton1010@gmail.com>
2020-09-04 19:01:11 +0000

    Mage a constant

--- main.swift
+++ main.swift
@@ -1,12 +1,11 @@
 // Declare a constant called friends to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
-
-
+let friends = 801
+print (friends)
 // Now assume you go through and remove friends that aren't active on social media. Attempt to update your friends constant to a lower number than it currently is. Observe what happens and then move to the next step.
-
-
+let friends = 112
+print(friends)
 // Does the above code compile? Why not? Print your explanation to the console using the print function. Go back and delete your line of code that updates the friends constant to a lower number so that the playground will compile properly.
-
-
+ did work
 // Note: These exercises reinforce Swift concepts in the context of a fitness tracking app.
 // Your fitness tracking app needs to know goal number of steps per day. Create a constant goalSteps and set it to 10000.
 
