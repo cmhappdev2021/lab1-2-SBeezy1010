a83490023549d5f9a5aca16682982cc5198351fc
SBeezy1010 <70169712+SBeezy1010@users.noreply.github.com>
2020-09-06 14:21:30 -0500

    added more constants and variables

--- main.swift
+++ main.swift
@@ -1,4 +1,4 @@
-// Declare a constant called friends to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
+/// Declare a constant called friends to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
 let friends = 801
 print (friends)
 // Now assume you go through and remove friends that aren't active on social media. Attempt to update your friends constant to a lower number than it currently is. Observe what happens and then move to the next step.
@@ -17,13 +17,13 @@ var schooling = (11)
 // Now imagine you just completed an additional year of school, and update the schooling variable accordingly. Print schooling to the console.
 var schooling = (12)
 // Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the print function.
-
+print(it should be different from constants...constants cant change)
 
 // Create a variable called steps that will keep track of the number of steps you take throughout the day. Set its initial value to 0 to represent the step count first thing in the morning. Print steps to the console.
-
-
+var steps = 0
+ptint = var steps
 // Now assume the tracker has been keeping track of steps all morning, and you want to show the user the latest step count. Update steps to be 2000. Print steps to the console. Then print "Good job! You're well on your way to your daily goal."
-
+var goalStep = 2000
 
 /* Imagine you're creating a simple photo sharing app. You want to keep track of the following metrics for each post:
 
@@ -35,7 +35,11 @@ Day created: The day of the month the post was created
 
 For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
 */
-
+var likes = 50
+var comments = 20
+let year = 2005
+let month = 3
+let day = 14
 
 /* There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
 
@@ -45,44 +49,52 @@ Number of steps taken today: The number of steps that a user has taken today
 Goal number of steps: The user's goal for number of steps to take each day
 Average heart rate: The user's average heart rate over the last 24 hours
 */
-
+let name = Supreme
+Names don´t change
+Var age = 15
+age changes
+Var number of steps = 3000
+steps change
+let goal number of steps = 2500
+it doesn change
+average heart rate = 92 
+this can change
 // Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
 
 
 // Type Safety
 // Declare two variables, one called firstDecimal and one called secondDecimal. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
-
-
+Var firstGecimal = 1.2
+Var secondDecimal = 1.3
 // Declare a variable called trueOrFalse and give it a boolean value. Try to assign it to firstDecimal like so: firstDecimal = trueOrFalse. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+Var trueOrfalse  = firstDecimal
 
 // Declare a variable and give it a string value. Then try to assign it to firstDecimal. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+Var = (¨firstDecimal¨)
+print(¨you can not keep changing the calue if something is the same value?
 // Finally, declare a variable with a whole number value. Then try to assign it to firstDecimal. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var 1 = firstDecimal
 
 // You have declared a number of constants and variables to keep track of fitness information. Declare one more variable with a boolean value called hasMetStepGoal.
-
-
+Var hasStepGoal = 21
+let hasStepGoal = 11
 // When you declared a constant for goal number of steps and a variable for current step count, you likely assigned each a value in the thousands. This can be difficult to read. Redeclare this constant and variable and, when assigning each a value in the thousands, format the number so that it is more readable.
-
+var hasStepGoal = 21_000
 
 // Declare a variable called name of type String, but do not give it a value. Print name to the console. Does the code compile? Remove any code that will not compile.
-
-
+var name of type String 
+no it doesn compile
 // Now assign a value to name, and print it to the console.
-
+vsr name = Supreme
 
 // Declare a variable called distanceTraveled and set it to 0. Do not give it an explicit type.
-
+var = distanceTraveled
 
 // Now assign a value of 54.3 to distanceTraveled. Does the code compile? Go back and set an explicit type on distanceTraveled so the code will compile.
-
+distnceTraceled = 23
 
 // You decide that your fitness tracking app should show the user what percentage of his/her goal has been achieved so far today. Declare a variable called percentCompleted and set it to 0. Do not explicity assign it a type.
-
+war percentCompleted = 8
 
 // Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. This means he/she is 34.67% of the way to his/her goal. Assign 34.67 to percentCompleted. Does the code compile? Go back and explicity assign a type to percentCompleted that will allow the code to compile.
-
-
+percentXompleted = 34.67
